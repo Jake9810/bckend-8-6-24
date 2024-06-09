d9dc27e3d6410a3ccdbb40ab799d5a9a009f2228
Jacob <jacobo.rodriguez499@gmail.com>
2024-06-09 00:58:40 +0000

    update

--- proyecto/index.js
+++ proyecto/index.js
@@ -11,7 +11,7 @@ app.listen(port,()=>{
   para crear queries se usa ?
   /clientes/invoices/taxes?minvalue=100&maxvalue=200
 
-  para especificar dentro de toda la entidad a cual me estoy refiriendo se usa :
+  (parametros) para especificar dentro de toda la entidad a que recurso me estoy refiriendo se usa :
   /clientes/:id/invoices/taxes/:invoiceId
   se puede hacer una busqueda más extensa de la siguiente manera
   /clientes/:id/invoices/taxes/:invoiceId/taxis/:taxId
